35fa072febcd9cd540d57a6a60068c457d1947d2
Murali <muralikrishna.kurucheti@gmail.com>
2017-08-05 12:46:50 +0530

    initial commit

---
+++ src/com/demo/GradleDemo.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import org.joda.time.LocalTime;
+
+public class GradleDemo {
+
+	public static void main(String[] args) {
+		
+		LocalTime time = new LocalTime();
+		
+		System.out.println("Local time is:"+time);
+		
+		System.out.println("Hello Gradle!");
+
+	}
+
+}
